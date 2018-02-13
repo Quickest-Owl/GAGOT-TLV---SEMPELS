4ca0a551c3c8812db8992da5fa7be3ae74b63ed5
redsoft <redsoftk@gmail.com>
2018-02-14 01:49:59 +0200

    Add imshow to rs-capture sample, start Playfile sample

--- RealSense/capture/rs-capture.cpp
+++ RealSense/capture/rs-capture.cpp
@@ -1,6 +1,7 @@
 // License: Apache 2.0. See LICENSE file in root directory.
 // Copyright(c) 2017 Intel Corporation. All Rights Reserved.
 
+#include <opencv2/opencv.hpp>   // Include OpenCV API
 #include <librealsense2/rs.hpp> // Include RealSense Cross Platform API
 #include "example.hpp"          // Include short list of convenience functions for rendering
 
@@ -21,7 +22,10 @@ int main(int argc, char * argv[]) try
     // Start streaming with default recommended configuration
     pipe.start();
 
-    while(app) // Application still alive?
+	const auto window_name = "Display Image";
+	cv::namedWindow (window_name, cv::WINDOW_AUTOSIZE);
+
+	while(app) // Application still alive?
     {
         rs2::frameset data = pipe.wait_for_frames(); // Wait for next set of frames from the camera
 
@@ -31,6 +35,16 @@ int main(int argc, char * argv[]) try
         // Render depth on to the first half of the screen and color on to the second
         depth_image.render(depth, { 0,               0, app.width() / 2, app.height() });
         color_image.render(color, { app.width() / 2, 0, app.width() / 2, app.height() });
+
+		// Query frame size (width and height)
+		const int w = depth.as<rs2::video_frame> ().get_width ();
+		const int h = depth.as<rs2::video_frame> ().get_height ();
+
+		// Create OpenCV matrix of size (w,h) from the colorized depth data
+		cv::Mat image (cv::Size (w, h), CV_8UC3, (void*)depth.get_data(), cv::Mat::AUTO_STEP);
+
+		// Update the window with new data
+		cv::imshow (window_name, image);
     }
 
     return EXIT_SUCCESS;
